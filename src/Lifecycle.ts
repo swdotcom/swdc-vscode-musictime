d365fb399c196052e27e0eae836b2376026aa837
xavluiz <xavluiz@gmail.com>
2020-09-14 12:38:57 -0700

    removed lifecycle for now

--- src/Lifecycle.ts
+++
@@ -1,9 +0,0 @@
-import { TrackerManager } from "./managers/TrackerManager";
-
-async function uninstall() {
-  const tracker: TrackerManager = TrackerManager.getInstance();
-  await tracker.trackEditorAction("editor", "deactivate");
-  process.exit(0);
-}
-
-uninstall();
