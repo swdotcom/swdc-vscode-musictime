4abadcd1136566c336e77b7979aa655adee03a68
Xavier Luiz <xavluiz@gmail.com>
2021-03-23 21:18:33 -0700

    check if its a slack integration before using the slack webclient identity

--- src/managers/IntegrationManager.ts
+++ src/managers/IntegrationManager.ts
@@ -28,26 +28,34 @@ async function updateIntegrations(user, name) {
     if (isActive) {
       // get the team domain and name if this is a slack integration
       if (!isFound) {
-        // get the workspace domain using the authId
-        const web = new WebClient(integration.access_token);
-        try {
-          const usersIdentify = await web.users.identity().catch((e) => {
+        if (name.toLowerCase() === "slack") {
+          // get the workspace domain using the authId
+          const web = new WebClient(integration.access_token);
+          try {
+            const usersIdentify = await web.users.identity().catch((e) => {
+              console.log("error fetching slack team info: ", e.message);
+              return null;
+            });
+            if (usersIdentify) {
+              // usersIdentity returns
+              // {team: {id, name, domain, image_102, image_132, ....}...}
+              // set the domain
+              integration["team_domain"] = usersIdentify?.team?.domain;
+              integration["team_name"] = usersIdentify?.team?.name;
+              // add the new integration
+              integrations.push(integration);
+              foundNewIntegration = true;
+            }
+          } catch (e) {
             console.log("error fetching slack team info: ", e.message);
-            return null;
-          });
-          if (usersIdentify) {
-            // usersIdentity returns
-            // {team: {id, name, domain, image_102, image_132, ....}...}
-            // set the domain
-            integration["team_domain"] = usersIdentify?.team?.domain;
-            integration["team_name"] = usersIdentify?.team?.name;
-            integrations.push(integration);
-            foundNewIntegration = true;
           }
-        } catch (e) {
-          console.log("error fetching slack team info: ", e.message);
+        } else {
+          // add the new integration
+          integrations.push(integration);
+          foundNewIntegration = true;
         }
       } else {
+        // add the existing
         integrations.push(integration);
       }
     }
