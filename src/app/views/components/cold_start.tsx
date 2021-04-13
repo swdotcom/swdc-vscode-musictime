d8686406e4b57c96a12a30f28795ebde97181b90
Xavier Luiz <xavluiz@gmail.com>
2021-04-13 15:20:45 -0700

    created new build with latest changes

--- src/app/views/components/cold_start.tsx
+++ src/app/views/components/cold_start.tsx
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import { BeakerIcon, MuiShareIcon, MuiSearchIcon, PawIcon, MuiPlayCircleOutlineIcon, MuiSubscriptionsIcon } from "../icons";
 import Card from "@material-ui/core/Card";
